5bcc0a67079b909ccf352890a9733ea5cf428cee
Daniel Mora <47727293+danielmora240396@users.noreply.github.com>
2022-04-02 23:38:07 -0600

    ESLint disable

--- src/Components/App.js
+++ src/Components/App.js
@@ -68,6 +68,7 @@ function App() {
     setState(tempState);
     setEnteredLetter('')
     } 
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [enteredLetter])
 
   const getWord = () => {
